527ed5f34d66038cbad705262d707aec1c305be6
Tibor Szasz <tszasz@gmail.com>
2018-09-25 15:38:13 +0200

    feat(app): works according to spec

--- src/index.js
+++ src/index.js
@@ -5,29 +5,61 @@ import * as moment from 'moment';
 // document.getElementById('content').innerText = JSON.stringify(callHistory);
 
 class ContactsWidget {
-    constructor(contacts, timeHelper) {
-        this.moment = timeHelper();
+    constructor(contacts) {
         this.root = document.querySelector('section');
-        this.render(contacts);
+        this.render(this.sortContacts(this.extractContactsFromHistory(contacts)));
     }
 
     render(contacts) {
         this.root.innerHTML = '';
         contacts.forEach(contact => {
-            const dateObj = this.formatTimestamp(contact.called);
+            const dateObj = this.formatTimestamp(contact.lastCalled);
             this.root.innerHTML += this.getRowHTML(contact, dateObj);
         });
         this.addRowEvents();
     }
 
-    sortContacts(contacts) {
+    extractContactsFromHistory(callHistory) {
+        const contacts = new Map();
+
+        callHistory.forEach(call => {
+            if (!contacts.get(call.phoneNumber)) {
+                const { firstName, lastName, phoneNumber } = call;
+                const contactData = {
+                    firstName,
+                    lastName,
+                    phoneNumber,
+                    lastCalled: call.called,
+                    callCount: 1,
+                };
+                contacts.set(call.phoneNumber, contactData);
+            } else {
+                const detail = contacts.get(call.phoneNumber);
+                detail.callCount++;
+                detail.called = (detail.called < call.called) ? call.called : detail.called;
+                contacts.set(call.phoneNumber, detail);
+            }
+        });
         return contacts;
+    }
+
+    sortContacts(contacts) {
+        const sortAlgo = (a, b) => {
+            let res = 0;
+            if (b[1].callCount !== a[1].callCount) {
+                res = b[1].callCount - a[1].callCount;
+            } else {
+                res = b[1].lastCalled - a[1].lastCalled;
+            }
+            return res;
+        }
+        return new Map([...contacts.entries()].sort(sortAlgo));
     }
 
     addRowEvents() {
         const contacts = this.root.querySelectorAll('article');
         contacts.forEach(row => {
-            row.addEventListener('click', function() {
+            row.addEventListener('click', function () {
                 // I needed "this" to refer to the row element
                 this.classList.toggle('open');
             });
@@ -35,7 +67,7 @@ class ContactsWidget {
     }
 
     getRowHTML(contactData, dateObj) {
-        const {firstName, lastName, phoneNumber} = contactData;
+        const { firstName, lastName, phoneNumber } = contactData;
         return `<article>
             <div class="row"><p>${firstName}<strong>${lastName}</strong></p><time>${dateObj.timeago}</time></div>
             <div class="details"><p>Number: ${phoneNumber} <em>last call: ${dateObj.dateTime}</em></p></div>
@@ -44,10 +76,10 @@ class ContactsWidget {
 
     formatTimestamp(timestamp) {
         return {
-            dateTime: moment.unix(timestamp).format("MMM d, YYYY"), // 'Aug 8, 2018',
+            dateTime: moment.unix(timestamp).format("MMM Do, YYYY"), // 'Aug 8, 2018',
             timeago: moment.unix(timestamp).fromNow() //'2 day ago'
         }
     }
 }
 
-const app = new ContactsWidget(callHistory, moment);
+const app = new ContactsWidget(callHistory);
